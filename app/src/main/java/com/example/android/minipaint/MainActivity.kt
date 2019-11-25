76f706c3f07f0d171064eec891ab4442e098e3c2
Peter Treitler <peter.treitler@axtesys.at>
2019-11-25 17:15:43 +0100

    Create empty canvas with background color

--- app/src/main/java/com/example/android/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.android.minipaint
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val myCanvasView = MyCanvasView(this).apply {
+            systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+            contentDescription = getString(R.string.canvas_content_description)
+        }
+
+        setContentView(myCanvasView)
     }
 }
