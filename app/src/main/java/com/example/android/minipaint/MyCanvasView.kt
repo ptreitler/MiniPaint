94d39caf9a320010e26ea8f1861615509a82f2d2
Peter Treitler <peter.treitler@axtesys.at>
2019-11-27 16:03:02 +0100

    Handle touch events to draw

--- app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -3,14 +3,42 @@ package com.example.android.minipaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
+import kotlin.math.abs
+
+private const val STROKE_WIDTH = 12f
 
 class MyCanvasView(context: Context) : View(context) {
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val paintColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    private val paint = Paint().apply {
+        color = paintColor
+        isAntiAlias = true // Smooths out edges of what is drawn without affecting shape
+        isDither = true // Dithering affects how colors are down-sampled
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline width (really thin)
+    }
+
+    private val path = Path()
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
@@ -24,5 +52,53 @@ class MyCanvasView(context: Context) : View(context) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+
+        return true
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+
+        if (dx >= touchTolerance || dy > touchTolerance) {
+            // quadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1) and ending at (x2,y2)
+            path.quadTo(
+                currentX,
+                currentY,
+                (motionTouchEventX + currentX) / 2,
+                (motionTouchEventY + currentY) / 2
+            )
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+
+            // Draw the path in the extra Canvas to cache it
+            extraCanvas.drawPath(path, paint)
+        }
+
+        invalidate()
+    }
+
+    private fun touchUp() {
+        // Reset the path so it doesn't get drawn again
+        path.reset()
     }
 }
